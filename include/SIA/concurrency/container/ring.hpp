45edc72486b23db763c57f6fc0e91ec30e651eab
wasabiwafer <wasabiwafer@gmail.com>
2025-03-15 18:18:33 +0900

    concur ring : patch

--- include/SIA/concurrency/container/ring.hpp
+++ include/SIA/concurrency/container/ring.hpp
@@ -6,8 +6,8 @@
 
 #include "SIA/utility/align_wrapper.hpp"
 #include "SIA/utility/compressed_pair.hpp"
+#include "SIA/concurrency/utility/tools.hpp"
 #include "SIA/concurrency/utility/mutex.hpp"
-#include "SIA/concurrency/utility/semaphore.hpp"
 #include "SIA/concurrency/utility/quota.hpp"
 
 namespace sia
@@ -37,6 +37,7 @@ namespace sia
                 using allocator_t = std::allocator_traits<Allocator>::template rebind_alloc<value_t>;
                 using allocator_traits_t = std::allocator_traits<allocator_t>;
                 using composition_t = ring_detail::ring_composition<T>;
+            public:
                 compressed_pair<allocator_t, composition_t> m_compair;
 
                 constexpr composition_t& get_comp() noexcept { return this->m_compair.second(); }
@@ -62,7 +63,7 @@ namespace sia
                 constexpr size_t capacity(this auto&& self) noexcept { return Size; }
                 constexpr size_t size(this auto&& self) noexcept
                 {
-                    constexpr auto mem_order = stamps::memory_orders::relaxed_v;
+                    constexpr auto mem_order = stamps::memory_orders::acquire_v;
                     composition_t& comp = self.get_comp();
                     return comp.m_end->load(mem_order) - comp.m_begin->load(mem_order);
                 }
@@ -71,134 +72,131 @@ namespace sia
                 constexpr bool is_full(this auto&& self) noexcept
                 { return self.size() == self.capacity(); }
 
+                template <tags::wait Tag = tags::wait::busy>
                 constexpr bool try_pop_back(T& out) noexcept(noexcept(out = T()) && noexcept(value_t().~value_t()))
                 {
                     constexpr auto mem_order = stamps::memory_orders::acq_rel_v;
                     composition_t& comp = this->get_comp();
-                    if (!this->is_empty())
+                    quota q {comp.m_mu_con.ref(), tags::quota::try_take};
+                    if (q.is_own())
                     {
-                        quota q {comp.m_mu_con.ref(), tags::quota::try_take};
-                        if (q.is_own())
+                        if (!this->is_empty())
                         {
                             value_t* target_ptr = this->raw_address(comp.m_end->fetch_sub(1, mem_order) - 1);
-                            loop<true>(&value_t::has_value, target_ptr);
+                            loop<true, Tag>(&value_t::has_value, target_ptr);
                             out = target_ptr->value();
-                            allocator_traits_t::destroy(this->get_alloc(), target_ptr);
+                            target_ptr->reset();
                             return true;
                         }
                     }
                     return false;
                 }
 
-                template <typename... Tys>
+                template <tags::wait Tag = tags::wait::busy, typename... Tys>
                 constexpr bool try_emplace_back(Tys&&... args) noexcept(noexcept(T(std::forward<Tys>(args)...)))
                 {
                     constexpr auto mem_order = stamps::memory_orders::acq_rel_v;
                     composition_t& comp = this->get_comp();
-                    if (!this->is_full())
+                    quota q {comp.m_mu_pro.ref(), tags::quota::try_take};
+                    if (q.is_own())
                     {
-                        quota q {comp.m_mu_pro.ref(), tags::quota::try_take};
-                        if (q.is_own())
+                        if (!this->is_full())
                         {
                             value_t* target_ptr = this->raw_address(comp.m_end->fetch_add(1, mem_order));
-                            loop<false>(&value_t::has_value, target_ptr);
-                            allocator_traits_t::construct(this->get_alloc(), target_ptr, std::forward<Tys>(args)...);
+                            loop<false, Tag>(&value_t::has_value, target_ptr);
+                            target_ptr->emplace(std::forward<Tys>(args)...);
                             return true;
                         }
                     }
                     return false;
                 }
-                template <typename Ty>
-                constexpr bool try_push_back(const Ty& arg) noexcept(noexcept(this->try_emplace_back(arg)))
-                { return this->try_emplace_back(arg); }
-                
-                template <typename Ty>
-                constexpr bool try_push_back(Ty&& arg) noexcept(noexcept(this->try_emplace_back(std::move(arg))))
-                { return this->try_emplace_back(std::move(arg)); }
+
+                template <tags::wait Tag = tags::wait::busy>
+                constexpr bool try_push_back(const T& arg) noexcept(noexcept(this->try_emplace_back(arg)))
+                { return this->try_emplace_back<Tag>(arg); }
                 
                 template <tags::wait Tag = tags::wait::busy>
+                constexpr bool try_push_back(T&& arg) noexcept(noexcept(this->try_emplace_back(std::move(arg))))
+                { return this->try_emplace_back<Tag>(std::move(arg)); }
+                
+                template <tags::wait LoopTag = tags::wait::busy, tags::wait TryTag = tags::wait::busy>
                 constexpr void pop_back(T& out) noexcept(noexcept(this->try_pop_back(out)))
-                { loop<true, Tag>(&ring::try_pop_back, this, out); }
+                { loop<true, LoopTag>(&ring::try_pop_back<TryTag>, this, out); }
                 
-                template <tags::wait Tag = tags::wait::busy, typename Ty>
-                constexpr void push_back(const Ty& arg) noexcept(noexcept(this->try_push_back(arg)))
-                { loop<true, Tag>(&ring::try_push_back, this, arg); }
+                template <tags::wait LoopTag = tags::wait::busy, tags::wait TryTag = tags::wait::busy>
+                constexpr void push_back(const T& arg) noexcept(noexcept(this->try_push_back(arg)))
+                { loop<true, LoopTag>(static_cast<bool(ring::*)(const T&)>(&ring::try_push_back<TryTag>), this, arg); }
                 
-                template <tags::wait Tag = tags::wait::busy, typename Ty>
-                constexpr void push_back(Ty&& arg) noexcept(noexcept(this->try_push_back(std::move(arg))))
-                { loop<true, Tag>(&ring::try_push_back, this, std::move(arg)); }
+                template <tags::wait LoopTag = tags::wait::busy, tags::wait TryTag = tags::wait::busy>
+                constexpr void push_back(T&& arg) noexcept(noexcept(this->try_push_back(std::move(arg))))
+                { loop<true, LoopTag>(static_cast<bool(ring::*)(T&&)>(&ring::try_push_back<TryTag>), this, std::move(arg)); }
 
-                // constexpr T back() noexcept(noexcept(T(T())))
-                // {
-                //     constexpr auto mem_order = stamps::memory_orders::acquire_v;
-                //     composition_t& comp = this->get_comp();
-                //     quota q {comp.m_mu_con.ref()};
-                //     return this->raw_address(comp.m_end->load(mem_order) - 1)->value();
-                // }
-                // constexpr const T back() const noexcept(noexcept(T(T())))
-                // {
-                //     constexpr auto mem_order = stamps::memory_orders::acquire_v;
-                //     composition_t& comp = this->get_comp();
-                //     quota q {comp.m_mu_con.ref()};
-                //     return this->raw_address(comp.m_end->load(mem_order) - 1)->value();
-                // }
-
+                template <tags::wait LoopTag = tags::wait::busy, tags::wait TryTag = tags::wait::busy, typename... Tys>
+                constexpr void emplace_back(Tys&&... args) noexcept(noexcept(this->try_emplace_back(std::forward<Tys>(args)...)))
+                { loop<true, LoopTag>(&ring::try_emplace_back<TryTag, Tys...>, this, std::forward<Tys>(args)...); }
+                
+                template <tags::wait Tag = tags::wait::busy>
                 constexpr bool try_pop_front(T& out) noexcept(noexcept(out = T()) && noexcept(value_t().~value_t()))
                 {
                     constexpr auto mem_order = stamps::memory_orders::acq_rel_v;
                     composition_t& comp = this->get_comp();
-                    if (!this->is_empty())
+                    quota q {comp.m_mu_con.ref(), tags::quota::try_take};
+                    if (q.is_own())
                     {
-                        quota q {comp.m_mu_con.ref(), tags::quota::try_take};
-                        if (q.is_own())
+                        if (!this->is_empty())
                         {
                             value_t* target_ptr = this->raw_address(comp.m_begin->fetch_add(1, mem_order));
-                            loop<true>(&value_t::has_value, target_ptr);
+                            loop<true, Tag>(&value_t::has_value, target_ptr);
                             out = target_ptr->value();
-                            allocator_traits_t::destroy(this->get_alloc(), target_ptr);
+                            target_ptr->reset();
                             return true;
                         }
                     }
                     return false;
                 }
 
-                template <typename... Tys>
+                template <tags::wait Tag = tags::wait::busy, typename... Tys>
                 constexpr bool try_emplace_front(Tys&&... args) noexcept(noexcept(T(std::forward<Tys>(args)...)))
                 {
                     constexpr auto mem_order = stamps::memory_orders::acq_rel_v;
                     composition_t& comp = this->get_comp();
-                    if (!this->is_full())
+                    quota q {comp.m_mu_pro.ref(), tags::quota::try_take};
+                    if (q.is_own())
                     {
-                        quota q {comp.m_mu_pro.ref(), tags::quota::try_take};
-                        if (q.is_own())
+                        if (!this->is_full())
                         {
-                            value_t* target_ptr = this->raw_address(comp.m_front->fetch_sub(1, mem_order) - 1);
-                            loop<false>(&value_t::has_value, target_ptr);
-                            allocator_traits_t::construct(this->get_alloc(), target_ptr, std::forward<Tys>(args)...);
+                            value_t* target_ptr = this->raw_address(comp.m_begin->fetch_sub(1, mem_order) - 1);
+                            loop<false, Tag>(&value_t::has_value, target_ptr);
+                            target_ptr->emplace(std::forward<Tys>(args)...);
                             return true;
                         }
                     }
                     return false;
                 }
-                template <typename Ty>
-                constexpr bool try_push_front(const Ty& arg) noexcept(noexcept(this->try_emplace_front(arg)))
-                { return this->try_emplace_front(arg); }
-                
-                template <typename Ty>
-                constexpr bool try_push_front(Ty&& arg) noexcept(noexcept(this->try_emplace_front(std::move(arg))))
-                { return this->try_emplace_front(std::move(arg)); }
+
+                template <tags::wait Tag = tags::wait::busy>
+                constexpr bool try_push_front(const T& arg) noexcept(noexcept(this->try_emplace_front(arg)))
+                { return this->try_emplace_front<Tag>(arg); }
                 
                 template <tags::wait Tag = tags::wait::busy>
+                constexpr bool try_push_front(T&& arg) noexcept(noexcept(this->try_emplace_front(std::move(arg))))
+                { return this->try_emplace_front<Tag>(std::move(arg)); }
+                
+                template <tags::wait LoopTag = tags::wait::busy, tags::wait TryTag = tags::wait::busy>
                 constexpr void pop_front(T& out) noexcept(noexcept(this->try_pop_front(out)))
-                { loop<true, Tag>(&ring::try_pop_front, this, out); }
+                { loop<true, LoopTag>(&ring::try_pop_front<TryTag>, this, out); }
                 
-                template <tags::wait Tag = tags::wait::busy, typename Ty>
-                constexpr void push_front(const Ty& arg) noexcept(noexcept(this->try_push_front(arg)))
-                { loop<true, Tag>(&ring::try_push_front, this, arg); }
+                template <tags::wait LoopTag = tags::wait::busy, tags::wait TryTag = tags::wait::busy>
+                constexpr void push_front(const T& arg) noexcept(noexcept(this->try_push_front(arg)))
+                { loop<true, LoopTag>(static_cast<bool(ring::*)(const T&)>(&ring::try_push_front<TryTag>), this, arg); }
                 
-                template <tags::wait Tag = tags::wait::busy, typename Ty>
-                constexpr void push_front(Ty&& arg) noexcept(noexcept(this->try_push_front(std::move(arg))))
-                { loop<true, Tag>(&ring::try_push_front, this, std::move(arg)); }
+                template <tags::wait LoopTag = tags::wait::busy, tags::wait TryTag = tags::wait::busy>
+                constexpr void push_front(T&& arg) noexcept(noexcept(this->try_push_front(std::move(arg))))
+                { loop<true, LoopTag>(static_cast<bool(ring::*)(T&&)>(&ring::try_push_front<TryTag>), this, std::move(arg)); }
+
+                template <tags::wait LoopTag = tags::wait::busy, tags::wait TryTag = tags::wait::busy, typename... Tys>
+                constexpr void emplace_front(Tys&&... args) noexcept(noexcept(this->try_emplace_front(std::forward<Tys>(args)...)))
+                { loop<true, LoopTag>(&ring::try_emplace_front<TryTag, Tys...>, this, std::forward<Tys>(args)...); }
             };
         } // namespace spsc
 
