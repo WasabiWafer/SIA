686a0a3ee09fdbd59e919eaf4390a7fe84b7d35e
Wasabi Wafer <wasabiwafer@gmail.com>
2025-06-29 01:59:24 +0900

    container/tail : rewite task...WIP

--- include/SIA/container/tail.hpp
+++ include/SIA/container/tail.hpp
@@ -1,123 +1,199 @@
 #pragma once
 
-#include <utility>
 #include <memory>
 
-#include "SIA/internals/types.hpp"
 #include "SIA/utility/compressed_pair.hpp"
+#include "SIA/memory/allocator_converter.hpp"
 
 namespace sia
 {
     namespace tail_detail
     {
         template <typename T>
-        struct tail_data
+        struct tail_node
         {
-            tail_data* m_next;
-            T m_data[1];
+            tail_node* m_node;
+            T* m_data;
+
+            constexpr tail_node* next() noexcept { return m_node; }
+            constexpr T* ptr() noexcept { return m_data; }
+            constexpr T& ref() noexcept { return *m_data; }
+            // constexpr void set_data(T* new_ptr) noexcept { m_data = new_ptr; }
+            constexpr void set_node(tail_node* new_ptr) noexcept { m_node = new_ptr; }
+            template <typename Allocator>
+            constexpr void destroy_data(Allocator&& alloc) noexcept(std::is_nothrow_destructible_v<T>)
+            { std::allocator_traits<std::remove_reference_t<Allocator>>::destroy(std::forward<Allocator>(alloc), m_data); }
+            template <typename Allocator>
+            constexpr void deallocate_data(Allocator&& alloc) noexcept(std::is_nothrow_destructible_v<T>)
+            { std::allocator_traits<std::remove_reference_t<Allocator>>::deallocate(std::forward<Allocator>(alloc), m_data, 1); }
+        };
+
+        template <typename T>
+        struct tail_stock
+        {
+            private:
+                using node_t = tail_node<T>;
+                node_t* m_stock;
+
+            public:
+                constexpr tail_stock(node_t* arg) noexcept
+                    : m_stock{arg}
+                { }
+
+                constexpr bool is_empty() noexcept { return m_stock == nullptr; }
+                constexpr node_t* begin() noexcept { return m_stock; }
+
+                constexpr node_t* pull() noexcept
+                {
+                    node_t* ret = m_stock;
+                    m_stock = ret->next();
+                    ret->set_node(nullptr);
+                    return ret;
+                }
+
+                constexpr void push(node_t* arg) noexcept
+                {
+                    arg->set_node(m_stock);
+                    m_stock = arg;
+                }
         };
 
         template <typename T>
         struct tail_composition
         {
-            tail_data<T>* m_data;
-            tail_data<T>* m_buffer;
+            private:
+                using node_t = tail_node<T>;
+            public:
+                tail_node<T>* m_head;
+                tail_stock<T> m_stock;
+
+                constexpr node_t* next() noexcept { return m_head; }
+                constexpr void set_node(node_t* new_ptr) noexcept { m_head = new_ptr; }
         };
     } // namespace tail_detail
     
     template <typename T, typename Allocator = std::allocator<T>>
     struct tail
     {
-    private:
-        using composition_t = tail_detail::tail_composition<T>;
-        using tail_data_t = tail_detail::tail_data<T>;
-        using tail_allocator_t = std::allocator_traits<Allocator>::template rebind_alloc<tail_data_t>;
-        using tail_allocator_traits_t = std::allocator_traits<tail_allocator_t>;
+        private:
+            using value_type = T;
+            using allocator_type = Allocator;
+            using composition_t = tail_detail::tail_composition<T>;
+            using node_t = tail_detail::tail_node<T>;
+            using allocator_converter_t = allocator_converter<node_t, allocator_type>;
+            using converted_allocator_t = allocator_converter_t::converted_allocator_t;
 
-        compressed_pair<tail_allocator_t, composition_t> m_compair;
+            compressed_pair<allocator_type, composition_t> m_compair;
 
-    private:
-        constexpr tail_allocator_t& get_tail_allocator() noexcept { return this->m_compair.first(); }
-        constexpr composition_t& get_composition(this auto&& self) noexcept { return self.m_compair.second(); }
-        constexpr void deallocate_proc(this auto&& self, tail_data_t* ptr) noexcept(noexcept(tail_allocator_traits_t::deallocate(self.get_tail_allocator(), ptr, 1)) && noexcept(T().~T()))
-        {
-            tail_allocator_t& t_alloc = self.get_tail_allocator();
-            while(ptr != nullptr)
+            constexpr allocator_type& get_allocator() noexcept { return m_compair.first(); }
+            constexpr const allocator_type& get_allocator() const noexcept { return m_compair.first(); }
+            constexpr composition_t& get_composition() noexcept { return m_compair.second(); }
+            constexpr const composition_t& get_composition() const noexcept { return m_compair.second(); }
+
+            template <typename... Tys>
+            constexpr value_type* create_data(Tys&&... args)
             {
-                tail_data_t* next = ptr->m_next;
-                tail_allocator_traits_t::deallocate(t_alloc, ptr, 1);
-                ptr = next;
-            }
-        }
-    public:
-        constexpr tail(const tail_allocator_t& alloc = tail_allocator_t()) noexcept(noexcept(tail_allocator_t(alloc)))
-            : m_compair(splits::one_v, alloc, nullptr, nullptr)
-        { }
-
-        ~tail() noexcept(noexcept(deallocate_proc(m_compair.second().m_data)) && noexcept(T().~T()))
-        {
-            auto& comp = m_compair.second();
-            deallocate_proc(comp.m_data);
-            deallocate_proc(comp.m_buffer);
-        }
-
-        constexpr bool is_empty(this auto&& self) noexcept
-        {
-            composition_t& comp = self.get_composition();
-            return comp.m_data == nullptr;
-        }
-
-        template <typename... Tys>
-        constexpr void emplace_front(Tys&&... args) noexcept(noexcept(tail_allocator_traits_t::allocate(this->get_tail_allocator(), 1)) && noexcept(T(std::forward<Tys>(args)...)))
-        {
-            tail_allocator_t& t_alloc = this->get_tail_allocator();
-            composition_t& comp = this->get_composition();
-
-            tail_data_t* new_block;
-
-            if (comp.m_buffer != nullptr) 
-            {
-                new_block = comp.m_buffer;
-                comp.m_buffer = comp.m_buffer->m_next;
-            }
-            else
-            {
-                new_block = tail_allocator_traits_t::allocate(t_alloc, 1);
+                value_type* ret = std::allocator_traits<allocator_type>::allocate(get_allocator(), 1);
+                std::allocator_traits<allocator_type>::construct(get_allocator(), ret, std::forward<Tys>(args)...);
+                return ret;
             }
 
-            std::construct_at(new_block->m_data, std::forward<Tys>(args)...);
-            new_block->m_next = comp.m_data;
-            comp.m_data = new_block;
-        }
-        constexpr void push_front(const T& arg) noexcept(noexcept(this->emplace_front(arg))) { this->emplace_front(arg); }
-        constexpr void push_front(T&& arg) noexcept(noexcept(this->emplace_front(std::move(arg)))) { this->emplace_front(std::move(arg)); }
-
-        constexpr void pop_front() noexcept(noexcept(T().~T()))
-        {
-            tail_allocator_t& t_alloc = this->get_tail_allocator();
-            composition_t& comp = this->get_composition();
-            if (!this->is_empty())
+            constexpr node_t* create_node(node_t* next_node, value_type* data_ptr)
             {
-                tail_data_t* out = comp.m_data;
-                comp.m_data = comp.m_data->m_next;
-                out->m_next = comp.m_buffer;
-                comp.m_buffer = out;
-                std::destroy_at(out->m_data);
+                allocator_converter_t ac {get_allocator()};
+                node_t* ret = std::allocator_traits<converted_allocator_t>::allocate(ac.get_allocator(), 1);
+                std::allocator_traits<converted_allocator_t>::construct(ac.get_allocator(), ret, next_node, data_ptr);
+                return ret;
             }
-        }
 
-        [[nodiscard]]
-        constexpr T& front() noexcept
-        {
-            composition_t& comp = this->get_composition();
-            return *(comp.m_data->m_data);
-        }
+            constexpr void deallocate_node(node_t* target) noexcept(std::is_nothrow_destructible_v<T>)
+            {
+                while (target != nullptr)
+                {
+                    node_t* tmp = target->next();
+                    target->deallocate_data(get_allocator());
+                    {
+                        allocator_converter_t ac {get_allocator()};
+                        std::allocator_traits<converted_allocator_t>::deallocate(ac.get_allocator(), target, 1);
+                    }
+                    target = tmp;
+                }
+            }
 
-        [[nodiscard]]
-        constexpr const T& front() const noexcept
-        {
-            composition_t& comp = this->get_composition();
-            return *(comp.m_data->m_data);
-        }
+        public:
+            constexpr tail(const allocator_type& alloc = allocator_type{ }) noexcept 
+                : m_compair(splits::one_v, alloc, nullptr, nullptr)
+            { }
+
+            // constexpr tail(const tail& arg, const allocator_type& alloc = allocator_type{ })
+            //     : m_compair(splits::one_v, alloc, nullptr, nullptr)
+            // {
+                
+            // }
+
+            constexpr ~tail() noexcept(std::is_nothrow_destructible_v<T>)
+            {
+                composition_t& comp = get_composition();
+                deallocate_node(comp.m_stock.begin());
+                deallocate_node(comp.m_head);
+            }
+
+            constexpr bool is_empty(this auto&& self) noexcept { return self.get_composition().m_head == nullptr; }
+
+            constexpr node_t* begin() noexcept { return get_composition().m_head; }
+            constexpr const node_t* begin() const noexcept { return get_composition().m_head; }
+            
+            template <typename NodeType, typename... Tys>
+            constexpr void emplace_after(NodeType* at, Tys&&... args)
+            {
+                composition_t& comp {get_composition()};
+                if (comp.m_stock.is_empty())
+                {
+                    value_type* new_data = create_data(std::forward<Tys>(args)...);
+                    node_t* new_node = create_node(at->next(), new_data);
+                    at->set_node(new_node);
+                }
+                else
+                {
+                    node_t* new_node = comp.m_stock.pull();
+                    new_node->set_node(at->next());
+                    std::allocator_traits<allocator_type>::construct(get_allocator(), new_node->ptr(), std::forward<Tys>(args)...);
+                    at->set_node(new_node);
+                }
+            }
+
+            template <typename... Tys>
+            constexpr void emplace_front(Tys&&... args)
+            { emplace_after(&get_composition(), std::forward<Tys>(args)...); }
+
+            template <typename NodeType>
+            constexpr void pop_after(NodeType* at) noexcept(std::is_nothrow_destructible_v<T>)
+            {
+                composition_t& comp {get_composition()};
+                node_t* target = at->next();
+                at->set_node(target->next());
+                target->destroy_data(get_allocator());
+                comp.m_stock.push(target);
+            }
+
+            constexpr void pop_front() noexcept(std::is_nothrow_destructible_v<T>)
+            { pop_after(&get_composition()); }
+
+            template <typename NodeType>
+            constexpr void push_after(NodeType* at, const T& arg)
+            { emplace_after(at, arg); }
+
+            template <typename NodeType>
+            constexpr void push_after(NodeType* at, T&& arg)
+            { emplace_after(at, std::move(arg)); }
+
+            constexpr void push_front(const T& arg)
+            { emplace_after(&get_composition(), arg); }
+
+            constexpr void push_front(T&& arg)
+            { emplace_after(&get_composition(), std::move(arg)); }
+
+            constexpr value_type& front() noexcept { return get_composition().m_head->ref(); }
+            constexpr const value_type& front() const noexcept { return get_composition().m_head->ref(); }
     };
 } // namespace sia
---
+++ include/SIA/container/tail_old.hpp
@@ -0,0 +1,123 @@
+#pragma once
+
+#include <utility>
+#include <memory>
+
+#include "SIA/internals/types.hpp"
+#include "SIA/utility/compressed_pair.hpp"
+
+namespace sia
+{
+    namespace tail_detail
+    {
+        template <typename T>
+        struct tail_data
+        {
+            tail_data* m_next;
+            T m_data[1];
+        };
+
+        template <typename T>
+        struct tail_composition
+        {
+            tail_data<T>* m_data;
+            tail_data<T>* m_buffer;
+        };
+    } // namespace tail_detail
+    
+    template <typename T, typename Allocator = std::allocator<T>>
+    struct tail
+    {
+    private:
+        using composition_t = tail_detail::tail_composition<T>;
+        using tail_data_t = tail_detail::tail_data<T>;
+        using tail_allocator_t = std::allocator_traits<Allocator>::template rebind_alloc<tail_data_t>;
+        using tail_allocator_traits_t = std::allocator_traits<tail_allocator_t>;
+
+        compressed_pair<tail_allocator_t, composition_t> m_compair;
+
+    private:
+        constexpr tail_allocator_t& get_tail_allocator() noexcept { return this->m_compair.first(); }
+        constexpr composition_t& get_composition(this auto&& self) noexcept { return self.m_compair.second(); }
+        constexpr void deallocate_proc(this auto&& self, tail_data_t* ptr) noexcept(noexcept(tail_allocator_traits_t::deallocate(self.get_tail_allocator(), ptr, 1)) && noexcept(T().~T()))
+        {
+            tail_allocator_t& t_alloc = self.get_tail_allocator();
+            while(ptr != nullptr)
+            {
+                tail_data_t* next = ptr->m_next;
+                tail_allocator_traits_t::deallocate(t_alloc, ptr, 1);
+                ptr = next;
+            }
+        }
+    public:
+        constexpr tail(const tail_allocator_t& alloc = tail_allocator_t()) noexcept(noexcept(tail_allocator_t(alloc)))
+            : m_compair(splits::one_v, alloc, nullptr, nullptr)
+        { }
+
+        ~tail() noexcept(noexcept(deallocate_proc(m_compair.second().m_data)) && noexcept(T().~T()))
+        {
+            auto& comp = m_compair.second();
+            deallocate_proc(comp.m_data);
+            deallocate_proc(comp.m_buffer);
+        }
+
+        constexpr bool is_empty(this auto&& self) noexcept
+        {
+            composition_t& comp = self.get_composition();
+            return comp.m_data == nullptr;
+        }
+
+        template <typename... Tys>
+        constexpr void emplace_front(Tys&&... args) noexcept(noexcept(tail_allocator_traits_t::allocate(this->get_tail_allocator(), 1)) && noexcept(T(std::forward<Tys>(args)...)))
+        {
+            tail_allocator_t& t_alloc = this->get_tail_allocator();
+            composition_t& comp = this->get_composition();
+
+            tail_data_t* new_block;
+
+            if (comp.m_buffer != nullptr) 
+            {
+                new_block = comp.m_buffer;
+                comp.m_buffer = comp.m_buffer->m_next;
+            }
+            else
+            {
+                new_block = tail_allocator_traits_t::allocate(t_alloc, 1);
+            }
+
+            std::construct_at(new_block->m_data, std::forward<Tys>(args)...);
+            new_block->m_next = comp.m_data;
+            comp.m_data = new_block;
+        }
+        constexpr void push_front(const T& arg) noexcept(noexcept(this->emplace_front(arg))) { this->emplace_front(arg); }
+        constexpr void push_front(T&& arg) noexcept(noexcept(this->emplace_front(std::move(arg)))) { this->emplace_front(std::move(arg)); }
+
+        constexpr void pop_front() noexcept(noexcept(T().~T()))
+        {
+            tail_allocator_t& t_alloc = this->get_tail_allocator();
+            composition_t& comp = this->get_composition();
+            if (!this->is_empty())
+            {
+                tail_data_t* out = comp.m_data;
+                comp.m_data = comp.m_data->m_next;
+                out->m_next = comp.m_buffer;
+                comp.m_buffer = out;
+                std::destroy_at(out->m_data);
+            }
+        }
+
+        [[nodiscard]]
+        constexpr T& front() noexcept
+        {
+            composition_t& comp = this->get_composition();
+            return *(comp.m_data->m_data);
+        }
+
+        [[nodiscard]]
+        constexpr const T& front() const noexcept
+        {
+            composition_t& comp = this->get_composition();
+            return *(comp.m_data->m_data);
+        }
+    };
+} // namespace sia
